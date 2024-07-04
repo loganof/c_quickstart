e236720b0d9e58a29561ed4de54f01efb853cdd4
logan <loganpeng28@gmail.com>
2024-07-04 23:53:28 +0800

    update format

--- modern_c++/move_semantics.cpp
+++ modern_c++/move_semantics.cpp
@@ -1,8 +1,8 @@
 #include <type_traits>
 
 template <typename T>
-typename remove_reference<T>::type&&
-move(T&& param)
+typename remove_reference<T>::type &&
+move(T &&param)
 {
     using ReturnType = typename remove_reference<T>::type &&;
     return static_cast<ReturnType>(param);
